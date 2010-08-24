4ef05f3c48de28b218ec4f0418a626253fb2a72d
hongaar <hongaar@6dfd4d5d-8e2d-54c4-8dc7-ccffeb0105b3>
2010-08-24 22:55:07 +0000

    application dir refact

---
+++ private/application/config/Config_Base.php
@@ -0,0 +1,47 @@
+<?php
+
+class Config_Base
+{	
+	// Site parameters
+	public $ident				= "ajde";
+	public $sitename 			= "ajde open web framework";	
+	public $version 			= array(
+									"number" => "0",
+									"name" => "alpha"
+									);
+	public $homepageRoute		= "home.html";
+	public $defaultRouteParts	= array(
+									"module" => "home",
+									"action" => "view",
+									"format" => "html"
+									);       
+	public $lang 				= "en";
+	public $layout 				= "default";
+	
+	// Performance
+	public $compressResources	= true;
+	public $compressHtml		= true;
+	public $debug 				= false;
+	public $useCache			= true;
+	
+	// Database settings
+	public $db_host 			= "localhost";
+	public $db_user 			= "ajde";
+	public $db_password 		= "ajde";
+	public $db_db 				= "ajde";
+
+	// Which modules should we call on bootstrapping?
+	public $bootstrap			= array(
+									"Ajde_Exception_Handler",
+									"Ajde_Session",
+									);
+
+	function __construct()
+	{
+		$this->local_root = $_SERVER["DOCUMENT_ROOT"].str_replace("/index.php", "", $_SERVER["PHP_SELF"]);
+		$this->site_domain = $_SERVER["SERVER_NAME"];
+		$this->site_path = str_replace('index.php', '', $_SERVER["PHP_SELF"]);
+		$this->site_root = $this->site_domain . $this->site_path;
+	}
+	
+}
---
+++ private/application/modules/ajdesite/template/sign.phtml
@@ -0,0 +1,9 @@
+<?php
+/* @var $this Ajde_Template */
+$this->requireJsLibrary('jquery', '1.4');
+echo $this->includeModule("ajdesite/menu.html");
+?>
+
+<p>
+Sign my guestbook
+</p>
