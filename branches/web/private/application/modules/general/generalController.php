02a8b75bfc0547a6d04939a482320b983e0f9348
hongaar <hongaar@6dfd4d5d-8e2d-54c4-8dc7-ccffeb0105b3>
2010-08-30 00:15:05 +0000

    web

---
+++ branches/web/private/application/modules/general/generalController.php
@@ -0,0 +1,14 @@
+<?php 
+
+class General extends Ajde_Controller
+{
+	function menuDefault()
+	{
+		return $this->loadTemplate();
+	}
+	
+	function viewDefault()
+	{
+		return $this->loadTemplate();
+	}
+}
