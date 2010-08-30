64374da63a564a815e5635683a8db714dd655e4f
hongaar <hongaar@6dfd4d5d-8e2d-54c4-8dc7-ccffeb0105b3>
2010-08-30 14:05:03 +0000

    merge from branch web: require public resources

--- trunk/private/lib/Ajde/Component/Css.php
+++ trunk/private/lib/Ajde/Component/Css.php
@@ -10,12 +10,21 @@ class Ajde_Component_Css extends Ajde_Component_Resource
 	
 	public function process()
 	{
-		$this->requireResource(
-			Ajde_Resource_Local::TYPE_STYLESHEET,
-			$this->attributes['action'],
-			$this->attributes['format'],
-			$this->attributes['base'],
-			$this->attributes['position']
-		);
+		if (array_key_exists('action', $this->attributes)) {
+			$this->requireResource(
+				Ajde_Resource_Local::TYPE_STYLESHEET,
+				$this->attributes['action'],
+				$this->attributes['format'],
+				$this->attributes['base'],
+				$this->attributes['position']
+			);
+		} elseif (array_key_exists('filename', $this->attributes)) {
+			$this->requirePublicResource(
+				Ajde_Resource_Local::TYPE_STYLESHEET,
+				$this->attributes['filename'],
+				$this->attributes['position']
+			);
+		}
+		
 	}
 }
