713383aa73b0e35897052e7d99c15eadb680b46d
hongaar <hongaar@gmail.com>
2010-11-24 14:46:27 +0000

    linux and php < 5.3 compat

--- private/lib/Ajde/FS/Find.php
+++ private/lib/Ajde/FS/Find.php
@@ -5,6 +5,10 @@ class Ajde_FS_Find extends Ajde_Object_Static
 	public static function findFile($dir, $pattern)
 	{
 		$search = $_SERVER['DOCUMENT_ROOT'] . '/' . $dir . $pattern;
+		$result = glob($search);
+		if ($result === false) {
+			return false;
+		}
 		foreach (glob($search) as $filename) {
 			return $filename;
 		}
