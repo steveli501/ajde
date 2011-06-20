8e89a0ad2e0d2fedf9a0b1350db9df16c6ea117e
hongaar@gmail.com <hongaar@gmail.com@6dfd4d5d-8e2d-54c4-8dc7-ccffeb0105b3>
2011-06-20 13:11:10 +0000

    restructured component and debugger modules to _core module

---
+++ trunk/private/application/modules/_core/res/js/debugger/view.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	elements = $('body *:not(#ajdeDebugger)');
+	var lowest = 0;
+	for (var i = 0; i < elements.length; i++) {
+		bottom = $(elements[i]).offset().top + $(elements[i]).outerHeight();
+		if (bottom > lowest) {
+			lowest = bottom;
+		}
+	};
+	$('#ajdeDebugger').hide().css({top: lowest + 'px'}).fadeIn('slow');	
+})
