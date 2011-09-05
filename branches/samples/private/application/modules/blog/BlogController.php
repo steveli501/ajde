62943c7992aa29c226dbc70956248d0a48d23e34
hongaar@gmail.com <hongaar@gmail.com@6dfd4d5d-8e2d-54c4-8dc7-ccffeb0105b3>
2011-09-05 10:53:39 +0000

    blog sample

--- branches/samples/private/application/modules/blog/BlogController.php
+++ branches/samples/private/application/modules/blog/BlogController.php
@@ -6,6 +6,7 @@ class BlogController extends Ajde_Controller
     {
     	AjdeX_Model::register($this);
 		$blog = new BlogCollection();
+		$blog->orderBy('date', AjdeX_Query::ORDER_DESC);
 		$blog->load();
 		$this->getView()->assign('blog', $blog);
         return $this->render();
