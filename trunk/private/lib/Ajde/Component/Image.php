e01d8e4dc19d0bef257d662c09f71024b86080d8
hongaar@gmail.com <hongaar@gmail.com@6dfd4d5d-8e2d-54c4-8dc7-ccffeb0105b3>
2011-09-07 19:03:20 +0000

    Assign array of key/values to template - Ajde_Template::assignArray() Added option to get base64 string for image - Ajde_Image::getBase64() , also in Ajde_Component_Image Ajde_Http_Response::dieOnCode fix added Ajde_Document_Format_Body, like _Html but always uses the 'empty' layout

--- trunk/private/lib/Ajde/Component/Image.php
+++ trunk/private/lib/Ajde/Component/Image.php
@@ -11,16 +11,30 @@ class Ajde_Component_Image extends Ajde_Component
 	protected function _init()
 	{
 		return array(
+			'base64' => 'base64',
 			'filename' => 'html',
-			'output' => 'image' 
+			'output' => 'image'
 		);
 	}
 	
 	public function process()
 	{
 		switch($this->_attributeParse()) {
-		case 'html':
-			
+		case 'base64':			
+			$image = new Ajde_Image($this->attributes['filename']);
+			$image->setWidth($this->attributes['width']);
+			$image->setHeight($this->attributes['height']);
+			$image->setCrop($this->attributes['crop']);
+						
+			$controller = Ajde_Controller::fromRoute(new Ajde_Core_Route('_core/component:imageBase64'));
+			$controller->setImage($image);
+			$controller->setWidth(issetor($this->attributes['width'], null));
+			$controller->setHeight(issetor($this->attributes['height'], null));
+			$controller->setExtraClass(issetor($this->attributes['class'], ''));
+					
+			return $controller->invoke();
+			break;
+		case 'html':			
 			$image = new Ajde_Image($this->attributes['filename']);
 			$image->setWidth($this->attributes['width']);
 			$image->setHeight($this->attributes['height']);
