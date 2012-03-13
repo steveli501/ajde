f0387d663bf3f66fc7ef924ef0cc13abbee4cb63
hongaar <hongaar@gmail.com>
2012-03-13 21:57:42 +0000

    AjdeX renamed to Ajde and merged with trunk

--- private/application/modules/samples/structure.sql
+++ private/application/modules/samples/structure.sql
@@ -1,19 +1,43 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 3.4.5deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 29, 2011 at 03:34 AM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+-- Generation Time: Mar 13, 2012 at 10:56 PM
+-- Server version: 5.1.58
+-- PHP Version: 5.3.6-13ubuntu3.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ajde-samples`
+-- Database: `ajde`
 --
-CREATE DATABASE `ajde-samples` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `ajde-samples`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acl`
+--
+
+CREATE TABLE IF NOT EXISTS `acl` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` enum('user','usergroup') NOT NULL,
+  `user` int(10) unsigned DEFAULT NULL,
+  `usergroup` int(10) unsigned DEFAULT NULL,
+  `module` varchar(255) NOT NULL DEFAULT '*',
+  `action` varchar(255) NOT NULL DEFAULT '*',
+  `permission` enum('allow','own','deny') NOT NULL DEFAULT 'own',
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  KEY `usergroup` (`usergroup`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
 
 -- --------------------------------------------------------
 
@@ -23,8 +47,72 @@ USE `ajde-samples`;
 
 CREATE TABLE IF NOT EXISTS `blog` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `title` varchar(255) NOT NULL COMMENT 'Title',
+  `title` varchar(4) NOT NULL COMMENT 'Title',
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Date',
   `content` text NOT NULL COMMENT 'Article',
+  `image` varchar(255) DEFAULT NULL COMMENT 'Image',
+  `optional_field` varchar(255) DEFAULT NULL,
+  `user` int(10) unsigned NOT NULL COMMENT 'Owner',
+  `published` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Published',
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` text NOT NULL,
+  `usergroup` int(10) unsigned NOT NULL DEFAULT '1',
+  `email` varchar(255) DEFAULT NULL,
+  `fullname` text,
+  `secret` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `usergroup` (`usergroup`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usergroup`
+--
+
+CREATE TABLE IF NOT EXISTS `usergroup` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `acl`
+--
+ALTER TABLE `acl`
+  ADD CONSTRAINT `acl_ibfk_3` FOREIGN KEY (`user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `acl_ibfk_4` FOREIGN KEY (`usergroup`) REFERENCES `usergroup` (`id`);
+
+--
+-- Constraints for table `blog`
+--
+ALTER TABLE `blog`
+  ADD CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`usergroup`) REFERENCES `usergroup` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- private/application/modules/user/structure.sql
+++ private/application/modules/user/structure.sql
@@ -1,17 +1,62 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.10deb1
+-- version 3.4.5deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 09, 2011 at 11:44 AM
--- Server version: 5.1.54
--- PHP Version: 5.3.5-1ubuntu7.2
+-- Generation Time: Mar 13, 2012 at 10:56 PM
+-- Server version: 5.1.58
+-- PHP Version: 5.3.6-13ubuntu3.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ajde-samples`
+-- Database: `ajde`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acl`
+--
+
+CREATE TABLE IF NOT EXISTS `acl` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` enum('user','usergroup') NOT NULL,
+  `user` int(10) unsigned DEFAULT NULL,
+  `usergroup` int(10) unsigned DEFAULT NULL,
+  `module` varchar(255) NOT NULL DEFAULT '*',
+  `action` varchar(255) NOT NULL DEFAULT '*',
+  `permission` enum('allow','own','deny') NOT NULL DEFAULT 'own',
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  KEY `usergroup` (`usergroup`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blog`
+--
+
+CREATE TABLE IF NOT EXISTS `blog` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `title` varchar(4) NOT NULL COMMENT 'Title',
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Date',
+  `content` text NOT NULL COMMENT 'Article',
+  `image` varchar(255) DEFAULT NULL COMMENT 'Image',
+  `optional_field` varchar(255) DEFAULT NULL,
+  `user` int(10) unsigned NOT NULL COMMENT 'Owner',
+  `published` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Published',
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -19,7 +64,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
@@ -29,9 +73,10 @@ CREATE TABLE IF NOT EXISTS `user` (
   `fullname` text,
   `secret` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`,`email`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
   KEY `usergroup` (`usergroup`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=52 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -39,7 +84,6 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Table structure for table `usergroup`
 --
 
-DROP TABLE IF EXISTS `usergroup`;
 CREATE TABLE IF NOT EXISTS `usergroup` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -51,7 +95,142 @@ CREATE TABLE IF NOT EXISTS `usergroup` (
 --
 
 --
+-- Constraints for table `acl`
+--
+ALTER TABLE `acl`
+  ADD CONSTRAINT `acl_ibfk_3` FOREIGN KEY (`user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `acl_ibfk_4` FOREIGN KEY (`usergroup`) REFERENCES `usergroup` (`id`);
+
+--
+-- Constraints for table `blog`
+--
+ALTER TABLE `blog`
+  ADD CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`usergroup`) REFERENCES `usergroup` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 3.4.5deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 13, 2012 at 10:56 PM
+-- Server version: 5.1.58
+-- PHP Version: 5.3.6-13ubuntu3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ajde`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acl`
+--
+
+CREATE TABLE IF NOT EXISTS `acl` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` enum('user','usergroup') NOT NULL,
+  `user` int(10) unsigned DEFAULT NULL,
+  `usergroup` int(10) unsigned DEFAULT NULL,
+  `module` varchar(255) NOT NULL DEFAULT '*',
+  `action` varchar(255) NOT NULL DEFAULT '*',
+  `permission` enum('allow','own','deny') NOT NULL DEFAULT 'own',
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  KEY `usergroup` (`usergroup`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blog`
+--
+
+CREATE TABLE IF NOT EXISTS `blog` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `title` varchar(4) NOT NULL COMMENT 'Title',
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Date',
+  `content` text NOT NULL COMMENT 'Article',
+  `image` varchar(255) DEFAULT NULL COMMENT 'Image',
+  `optional_field` varchar(255) DEFAULT NULL,
+  `user` int(10) unsigned NOT NULL COMMENT 'Owner',
+  `published` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Published',
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` text NOT NULL,
+  `usergroup` int(10) unsigned NOT NULL DEFAULT '1',
+  `email` varchar(255) DEFAULT NULL,
+  `fullname` text,
+  `secret` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `usergroup` (`usergroup`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usergroup`
+--
+
+CREATE TABLE IF NOT EXISTS `usergroup` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `acl`
+--
+ALTER TABLE `acl`
+  ADD CONSTRAINT `acl_ibfk_3` FOREIGN KEY (`user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `acl_ibfk_4` FOREIGN KEY (`usergroup`) REFERENCES `usergroup` (`id`);
+
+--
+-- Constraints for table `blog`
+--
+ALTER TABLE `blog`
+  ADD CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`usergroup`) REFERENCES `usergroup` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
