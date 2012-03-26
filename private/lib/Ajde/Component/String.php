85f1bb831ac758e3ed784e7bcc863b661c96470c
hongaar <hongaar@gmail.com>
2012-03-26 10:50:05 +0000

    word trim escaping in default template shutdown function in index.php

--- private/lib/Ajde/Component/String.php
+++ private/lib/Ajde/Component/String.php
@@ -80,7 +80,36 @@ class Ajde_Component_String extends Ajde_Component
 			return self::clean($var);
 		}
 	}
+	
+	/**
+	* Cut string to n symbols and add delim but do not break words.
+	*
+	* Example:
+	* <code>
+	*  $string = 'this sentence is way too long';
+	*  echo neat_trim($string, 16);
+	* </code>
+	*
+	* Output: 'this sentence is...'
+	*
+	* @access public
+	* @param string string we are operating with
+	* @param integer character count to cut to
+	* @param string|NULL delimiter. Default: '...'
+	* @return string processed string
+	 * 
+	 * @see http://www.justin-cook.com/wp/2006/06/27/php-trim-a-string-without-cutting-any-words/
+	**/
+	public static function trim($str, $n, $delim = '...') {
+		$len = strlen($str);
+		if ($len > $n) {
+			$str = str_replace("\n","",$str);
+			$str = str_replace("\r","",$str);
+			$n = $n - strlen($delim);
+			preg_match('/(.{' . $n . '}.*?)\b/', $str, $matches);
+			return rtrim($matches[1]) . $delim;
+		} else {
+			return $str;
+		}
+	}
 }
-
-
-		
