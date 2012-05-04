1b60b6d52984174bbe66f77d91c2a8c0ec4b5c03
hongaar <hongaar@gmail.com>
2012-05-04 13:38:51 +0000

    crud

--- private/lib/Ajde/Crud/Field/Enum.php
+++ private/lib/Ajde/Crud/Field/Enum.php
@@ -8,6 +8,11 @@ class Ajde_Crud_Field_Enum extends Ajde_Crud_Field
 		$options = explode(',', $this->getLength());
 		foreach($options as $option) {
 			$option = trim($option, "'");
+			if ($this->hasFilter()) {
+				if (!in_array($option, $this->getFilter())) {
+					continue;
+				}
+			}
 			$return[$option] = $option;
 		}
 		return $return;
--- private/lib/Ajde/Crud/Field/Fk.php
+++ private/lib/Ajde/Crud/Field/Fk.php
@@ -33,7 +33,29 @@ class Ajde_Crud_Field_Fk extends Ajde_Crud_Field
 	}
 	
 	public function getValues()
-	{
+	{		
+		if ($this->hasFilter()) {
+			$filter = $this->getFilter();
+			$group = new Ajde_Filter_WhereGroup();
+			foreach($filter as $rule) {
+				$group->addFilter(new Ajde_Filter_Where($this->getModel()->getDisplayField(), Ajde_Filter::FILTER_EQUALS, $rule, Ajde_Query::OP_OR));
+			}
+			$this->getCollection()->addFilter($group);
+		}
+
+		if ($this->hasAdvancedFilter()) {
+			$filters = $this->getAdvancedFilter();
+			$group = new Ajde_Filter_WhereGroup();
+			foreach($filters as $filter) {
+				if ($filter instanceof Ajde_Filter_Where) {
+					$group->addFilter($filter);
+				} else {
+					$this->getCollection()->addFilter($filter);
+				}		
+			}
+			$this->getCollection()->addFilter($group);
+		}
+		
 		$this->getCollection()->orderBy($this->getModel()->getDisplayField());
 		$return = array();
 		foreach($this->getCollection() as $model) {
