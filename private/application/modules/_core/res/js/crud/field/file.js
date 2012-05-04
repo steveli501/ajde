b6fe43e819480c0c3271555a5bc8689c781797ce
hongaar <hongaar@gmail.com>
2012-05-04 17:08:54 +0000

    filebrowser

--- private/application/modules/_core/res/js/crud/field/file.js
+++ private/application/modules/_core/res/js/crud/field/file.js
@@ -7,4 +7,23 @@
 		$fileupload.removeClass('visuallyhidden');
 		return false;
 	});
+	
+	$('a.toggleFileBrowser').click(function(e) {
+		e.preventDefault();
+		$(this).next().slideToggle();
+	});
+	
+	$('div.browser > a').click(function(e) {
+		e.preventDefault();
+		$(this).parent().find('a').removeClass('active');
+		$(this).addClass('active');
+		var filename = $(this).find('span.filename').text();
+		$(this).parents('div.fileupload:eq(0)').find('input').val(filename);
+	});
+	
+	$('div.browser > a span.preview').click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		window.open($(this).attr('data-url'), 'preview');
+	})
 });
